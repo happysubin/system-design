f78946dfb4ac2174d1fbf55b6185a4d85cce4e20
happysubin <asb0711@gmail.com>
2026-03-27 21:40:18 +0900

    선착순 쿠폰 시스템 세팅

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-Hello, World!!!
+# 시스템 디자인 공부
+
+- 선착순 쿠폰 시스템 공부: first-come-coupon
---
+++ first-come-coupon/src/main/kotlin/com/firstcomecoupon/FirstComeCouponApplication.kt
@@ -0,0 +1,11 @@
+package com.firstcomecoupon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FirstComeCouponApplication
+
+fun main(args: Array<String>) {
+    runApplication<FirstComeCouponApplication>(*args)
+}
