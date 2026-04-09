b86813217039c24ce6a2b6ffa00070f42581dfe1
happysubin <asb0711@gmail.com>
2026-04-09 23:05:09 +0900

    chore: 코틀린 버전 2.3.0으로 업그레이드

--- pg-lab/build.gradle.kts
+++ pg-lab/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    kotlin("jvm") version "2.2.21"
-    kotlin("plugin.spring") version "2.2.21"
+    kotlin("jvm") version "2.3.0"
+    kotlin("plugin.spring") version "2.3.0"
     id("org.springframework.boot") version "4.0.5"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.2.21"
@@ -12,7 +12,7 @@ description = "pg-lab"
 
 java {
     toolchain {
-        languageVersion = JavaLanguageVersion.of(24)
+        languageVersion = JavaLanguageVersion.of(25)
     }
 }
 
